9a804b24be7d5f6ea280b23057c6442d13b98228
Andrew Josephus <andrewjosephus@gmail.com>
2022-05-12 10:59:06 +0800

    chore: changed code to render new logic

--- src/components/ProjectWriteups.tsx
+++ src/components/ProjectWriteups.tsx
@@ -10,80 +10,86 @@ import {
   WriteupWrapper,
   YellowLine,
 } from '../pages/styled';
-import { DataHolder, ProjectData, projectData } from './Data';
+import { ProjectData, projectData } from './Data';
 
 interface ProjectWriteupsProps {
   keywords: string[];
-  initialState?: DataHolder;
 }
 
 const ProjectWriteups: React.FC<ProjectWriteupsProps> = (props) => {
-  const { keywords, initialState = {} } = props;
-  const [data, setData] = React.useState<DataHolder>(() => initialState);
+  const { keywords } = props;
+  const [data, setData] = React.useState<any[]>(() => projectData);
 
   if (Object.keys(data).length === 0) {
     setData(projectData);
   }
 
   React.useEffect(() => {
-    const dataHolder: DataHolder = {};
-    const years = Object.keys(projectData);
-    years.reverse();
+    const dataHolder = [...projectData];
+    const years = Object.keys(projectData).reverse();
 
     keywords.forEach((keyword) => {
       let re = new RegExp(keyword, 'i');
+    });
 
-      years.forEach((year: string) => {
-        if (dataHolder[year] == null) {
-          dataHolder[year] = [];
-        }
-        projectData[year].forEach((obj: ProjectData) => {
-          if (dataHolder[year].includes(obj)) {
-            return;
-          }
-          if (obj['title'].search(re) == -1) {
-            if (obj['detail'].search(re) != -1) {
-              dataHolder[year].push(obj);
-            }
-          } else {
-            dataHolder[year].push(obj);
-          }
-        });
-        if (dataHolder[year].length == 0) {
-          delete dataHolder[year];
-        }
-      });
+    years.forEach((year: string) => {
+      // if (dataHolder[year] == null) {
+      //   dataHolder[year] = [];
+      // }
+
+      dataHolder[year].filter((obj) => {});
+
+      // projectData[year].forEach((obj: ProjectData) => {
+      //   if (dataHolder[year].includes(obj)) {
+      //     return;
+      //   }
+      //   if (obj['title'].search(re) == -1) {
+      //     if (obj['detail'].search(re) != -1) {
+      //       dataHolder[year].push(obj);
+      //     }
+      //   } else {
+      //     dataHolder[year].push(obj);
+      //   }
+      // });
+      if (dataHolder[year].length == 0) {
+        delete dataHolder[year];
+      }
     });
     setData(dataHolder);
   }, [keywords]);
 
-  const years = Object.keys(data);
-  years.reverse();
+  const years = [];
+  for (const i in data) {
+    years.push(data[i][0]);
+  }
+  const filteredYears = data.filter((yearItem) => yearItem[1].length > 0);
 
   return (
     <WriteupWrapper width={100}>
-      {years.map((year) => (
-        <>
-          <WriteupTitles marginBlockEnd={5} key={year}>
-            {year}
-          </WriteupTitles>
-          <RedLine></RedLine>
-          <Projectblocks>
-            <Projectlist>
-              {data[year].map(
-                (writeup: { link: string; title: string; detail: string }) => (
-                  <li key={writeup.title}>
-                    <StyledALink href={writeup['link']} target="_blank">
-                      {writeup['title']}
+      {years.map((year) => {
+        const newData = filteredYears.filter((obj) => obj[0] === year);
+        const projectData = newData[0][1];
+        return (
+          <>
+            <WriteupTitles marginBlockEnd={5} key={year}>
+              {year}
+            </WriteupTitles>
+            <RedLine></RedLine>
+            <Projectblocks>
+              <Projectlist>
+                {projectData.map((prjdata: ProjectData) => (
+                  <li key={prjdata['title']}>
+                    <StyledALink href={prjdata['link']} target="_blank">
+                      {prjdata['title']}
                     </StyledALink>
-                    <Projectwriteup>{writeup['detail']}</Projectwriteup>
+                    <Projectwriteup>{prjdata['detail']}</Projectwriteup>
                   </li>
-                )
-              )}
-            </Projectlist>
-          </Projectblocks>
-        </>
-      ))}
+                ))}
+              </Projectlist>
+            </Projectblocks>
+          </>
+        );
+      })}
 
       <YellowLine />
     </WriteupWrapper>
